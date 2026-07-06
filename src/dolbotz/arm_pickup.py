45e81a7e798e8e93b6401820e6a1f665f10ddb59
9o9hz <seonjuhan1@gmail.com>
2026-07-06 16:51:17 +0900

    모델 경로 및 탐지 클래스 기본값 설정

--- src/dolbotz/arm_pickup.py
+++ src/dolbotz/arm_pickup.py
@@ -37,8 +37,8 @@ class ArmPickupNode(Node):
     def __init__(self):
         super().__init__('arm_pickup_node')
 
-        self.declare_parameter('model_path', '')
-        self.declare_parameter('target_class', 'supply_box')
+        self.declare_parameter('model_path', '/home/j/dolbotZ/supplybest.pt')
+        self.declare_parameter('target_class', 'supplybox')
         self.declare_parameter('conf_threshold', 0.5)
         self.declare_parameter('depth_roi_radius', 5)
         self.declare_parameter('max_depth_m', 2.0)
